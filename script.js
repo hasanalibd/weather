440b12e74d6ce921502611d92c5288bb7e22a20a
hasanalibd <hasanali182838@gmail.com>
2023-06-20 13:51:13 +0600

    javascript function implement

---
+++ script.js
@@ -0,0 +1,26 @@
+const API_KEY = '2b113a2d612cd967e14d1f2a5a1331e0';
+
+const getWeatherData = (location) =>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${location}&appid=${API_KEY}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayTemperature(data))
+    
+}
+
+const displayTemperature = data =>{
+    const temperature = document.getElementById('temperature');
+    console.log(data);
+    temperature.innerText = data.main.temp;
+}
+
+
+document.getElementById('search-btn').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const location = searchField.value;
+    // set city in display
+    document.getElementById('city').innerText = location;
+    getWeatherData(location);
+})
+
+getWeatherData('Dhaka')
